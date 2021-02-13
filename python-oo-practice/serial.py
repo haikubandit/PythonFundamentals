e557bc058635a11e26f7779779c0a85232f2e671
Daniel Knapp <knapparatus@gmail.com>
2021-02-12 18:27:58 -0600

    completed python oop practice

--- python-oo-practice/serial.py
+++ python-oo-practice/serial.py
@@ -2,6 +2,13 @@
 
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
+
+    Attributes
+    -------------
+    start: int 
+        starting number
+    next: int
+        sequentially generated number
     
     >>> serial = SerialGenerator(start=100)
 
@@ -19,4 +26,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Initialize starting number"""
+        self.start = self.next = start
+    
+    # creates string description of the instance
+    def __repr__(self):
+        return f"SerialGenerator(start={self.start} next={self.next})"
+
+    def generate(self):
+        """Generate next consecutive number"""
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """Reset number to start number"""
+        self.next = self.start
+        # return self.next
 
--- python-oo-practice/wordfinder.py
+++ python-oo-practice/wordfinder.py
@@ -1,5 +1,46 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
 
 class WordFinder:
-    ...
+    """
+    A class used to get a random word from a dictionary file
+
+    Attributes
+    -------------
+    word_list: list 
+        list of all words in dictionary file
+
+    >>> w = WordFinder('test_dict.txt')
+    8 words read
+
+    >>> w.random() in ["specialism","specialist","specialistic","speciality","specialization","specialize","specialized","specializer"]
+    True
+    """
+
+    def __init__(self, filepath):
+        """Create list of word attributes"""
+        dictionary = open(filepath, "r")
+        self.word_list = self.generate_dict(dictionary)
+
+        print(f"{len(self.word_list)} words read")
+
+    def __repr__(self):
+        """creates string description of the instance"""
+        return f"WordFinder(filename={self.filename} file_object={self.file_object})"
+
+    def generate_dict(self, dictionary):
+        """Create list of all dictionary file words"""
+        return [word.strip() for word in dictionary]
+
+    def random(self):
+        """Randomly generate word from dictionary"""
+        return random.choice(self.word_list)
+
+
+class SpecialWordFinder(WordFinder):
+    """Special word finder that excludes blank lines and comments"""
+
+    def generate_dict(self, dictionary):
+            """Create list of all dictionary file words"""
+            return [word.strip() for word in dictionary if word.strip() and not word.startswith("#")]
